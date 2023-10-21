d21e71688dbad7fb885092c9b2491b43ea0e4401
Alexander Almaguer <almagueralexander839@gmail.com>
2023-10-21 19:54:12 -0400

    errores

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  let name = ''
-
-
-  res.render('index', {
-    title: 'Alexander Raul Almaguer Orta Ci. 30.626.438 Seccion 2',
-    name: name,
-  });
+  res.render('index', { title: 'Alexander Raul Almaguer Orta Ci. 30.626.438 seccion 2' });
 });
 
 module.exports = router;
