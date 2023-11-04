85b1c0b9f30d199396208cf2a82c1ab08d7bce81
Alexander Almaguer <almagueralexander839@gmail.com>
2023-11-03 23:36:07 -0400

    prueba 2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -11,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contacto', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,165 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+const db = require('../db/model')
 
-/* GET home page. */
+// Pagina principal
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alexander Raul Almaguer Orta Ci. 30.626.438 seccion 2' });
+  let name = 'Lavadoras portatiles'
+  res.render('index', {
+    title: 'Lavadoras portatiles',
+    name: name,
+  });
 });
+
+// Nosotros
+router.get('/nosotros', (req, res) => {
+  res.render('nosotros');
+});
+
+// Productos
+router.get('/productos', (req, res) => {
+  res.render('productos');
+});
+ 
+// Index
+router.get('/index', (req, res) => {
+  res.render('index');
+});
+
+// Contacto
+router.get('/contacto', (req, res) => {
+  res.render('contacto');
+});
+
+// Verificacion de usuario
+router.post('/login', function(req, res, next) {
+  let user = req.body.user
+  let pass = req.body.pass
+  if (user == process.env.usuario && pass == process.env.contrasena)  {
+      res.render('administracion');
+  } else {
+    res.render('contacto', { error: 'Datos incorrectos' });
+  }
+})
+
+
+// Listado de Productos
+router.get('/listado', (req, res) => {
+  db.obtenerProductos()
+  .then(data => {
+    console.log (data)
+    res.render('listado', {productos: data});
+  })
+  .catch(err => {
+    res.render('listado', {productos: []});
+  })
+});
+
+// Listado de categorias
+router.get('/listado-categorias', (req, res) => {
+  db.obtenerCategorias()
+  .then(data => {
+    console.log (data)
+    res.render('listado-categorias', {categorias: data});
+  })
+  .catch(err => {
+    res.render('listado-categorias', {categorias: []});
+  })
+});
+
+// Agregar producto
+router.get('/agregar-producto', (req, res) => {
+  db.obtenerCategorias()
+  .then(data => {
+    console.log (data)
+    res.render('agregar-producto', {categorias: data});
+  })
+  .catch(err => {
+    res.render('agregar-producto', {categorias: []});
+  })
+});
+router.post('/productos', (req, res) => {
+  const { nombre, codigo, laboratorio, precio, cantidad, descripcion, categoria_id } = req.body;
+  db.insertarProducto(nombre, codigo, laboratorio, precio, cantidad, descripcion, categoria_id); // Función que inserta el producto en la base de datos
+  res.redirect('/agregar-producto');
+});
+
+// Agregar categoria
+router.get('/agregar-categoria', (req, res) => {
+  db.obtenerCategorias()
+  .then(data => {
+    console.log (data)
+    res.render('agregar-categoria', {categorias: data});
+  })
+  .catch(err => {
+    res.render('agregar-categoria', {categorias: []});
+  })
+});
+router.post('/categoria', (req, res) => {
+  const {nombre} = req.body;
+  db.insertarCategoria(nombre); // Función que inserta el producto en la base de datos
+  res.redirect('/agregar-categoria');
+});
+
+// Editar categoria
+router.get('/editar-categorias/:id', (req, res) => {
+  const id = req.params.id;
+  db.obtenercategoriaid(id)
+  .then(data => {
+    console.log (data)
+    res.render('editar-categorias', {categorias: data});
+  })
+  .catch(err => {
+    res.render('editar-categorias', {categorias: []});
+  })
+});
+router.post('/editar', (req, res) => {
+  const id = req.params.id;
+  const nombre = req.body.nombre;
+  db.editarCategoria(id, nombre); // Función que inserta el producto en la base de datos
+  res.redirect('/listado-categorias');
+});
+
+// Editar producto
+router.get('/editar-producto/:id', (req, res) => {
+  const id = req.params.id;
+  db.obtenerproductoid(id) 
+  .then(data => {
+    console.log (data)
+    res.render('editar-producto', {productos: data});
+  })
+  .catch(err => {
+    res.render('editar-producto', {productos: []});
+  })
+});
+router.post('/actualizar', (req, res) => {
+  const id = req.params.id;
+  const { nombre, codigo, laboratorio, precio, cantidad, descripcion} = req.body;
+  db.editarProducto(id, nombre, codigo, laboratorio, precio, cantidad, descripcion); // Función que inserta el producto en la base de datos
+  res.redirect('/listado');
+});
+
+// Agregar imagen
+router.get('/agregar-imagen/:id', (req, res) => {
+  db.obtenerCategorias()
+  .then(data => {
+    console.log (data)
+    res.render('agregar-imagen', {categorias: data});
+  })
+  .catch(err => {
+    res.render('agregar-categoria', {categorias: []});
+  })
+});
+router.post('/imagen', (req, res) => {
+  const {url, producto_id, destacado} = req.body;
+  db.insertarImagen(url, producto_id, destacado); // Función que inserta el producto en la base de datos
+  res.redirect('/listado');
+});
+
+
+
+
+
 
 module.exports = router;
