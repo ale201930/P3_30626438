f543eef4528b3da4a8ebc09be44f835326fdb87f
Alexander Almaguer <almagueralexander839@gmail.com>
2023-11-10 20:36:10 -0400

    solucion

--- routes/index.js
+++ routes/index.js
@@ -1,163 +1,256 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
-const db = require('../db/model')
+const db = require('../db/models');
+require('dotenv').config()
 
-// Pagina principal
-router.get('/', function(req, res, next) {
-  let name = 'Lavadoras portatiles'
-  res.render('index', {
-    title: 'Lavadoras portatiles',
-    name: name,
-  });
-});
 
-// Nosotros
+
+router.get('/', (req, res) => {
+  res.render('vista1')
+} )
+
 router.get('/nosotros', (req, res) => {
-  res.render('nosotros');
-});
+  res.render('nosotros')
+} )
 
-// Productos
-router.get('/productos', (req, res) => {
-  res.render('productos');
-});
- 
-// Index
-router.get('/index', (req, res) => {
-  res.render('index');
-});
+router.get('/login', (req, res) => {
+  res.render('login')
+} )
 
-// Contacto
-router.get('/contacto', (req, res) => {
-  res.render('contacto');
-});
 
-// Verificacion de usuario
-router.post('/login', function(req, res, next) {
+router.post('/login', (req,res) =>{
   let user = req.body.user
   let pass = req.body.pass
-  if (user == process.env.usuario && pass == process.env.contrasena)  {
-      res.render('administracion');
+  if (user == process.env.usuario && pass == process.env.contrasena) {
+    res.render('administrar')
   } else {
-    res.render('contacto', { error: 'Datos incorrectos' });
+   res.render('login', { error: 'Datos incorrectos' });
   }
 })
 
-
-// Listado de Productos
-router.get('/listado', (req, res) => {
-  db.obtenerProductos()
-  .then(data => {
-    console.log (data)
-    res.render('listado', {productos: data});
+//productos
+router.get('/productos', (req, res) => {
+  db.getproducto()
+    .then(data => {        
+      console.log(data)
+      res.render('productos', { producto: data });
   })
   .catch(err => {
-    res.render('listado', {productos: []});
+      res.render('productos', { producto: [] });
+  })
+
+  
+
+});
+
+
+
+router.get('/categorias', (req, res) => {
+  db.getcategory()
+    .then(data => {        
+      console.log(data)
+      res.render('categorias', { category: data });
+  })
+  .catch(err => {
+      res.render('categorias', { category: [] });
+  })
+
+});
+
+router.get('/imagenes', (req, res) => {
+  db.getimagen()
+    .then(data => {        
+      console.log(data)
+      res.render('imagenes', { imagen: data });
+  })
+  .catch(err => {
+      res.render('imagenes', { imagen: [] });
+  })
+
+});
+
+
+router.get('/insertcat', (req, res) => {
+  res.render('insertcat')
+} )
+
+router.get('/insertima', (req, res) => {
+  res.render('insertima')
+} )
+
+//insertar producto
+router.get('/insert_producto', (req, res) => {
+  res.render('insert_producto')
+} )
+
+
+router.post('/insert_producto', (req, res) => {
+  const {code, name, lab, quantity, description, price, category_id} = req.body;
+  console.log(code, name, lab, quantity, description, price, category_id);
+  db.insertproducto(code, name,lab,quantity,description,price,category_id)
+  .then(() => {
+     res.redirect('productos')
+  })
+  .catch(err => {
+    console.log(err);
   })
 });
 
-// Listado de categorias
-router.get('/listado-categorias', (req, res) => {
-  db.obtenerCategorias()
-  .then(data => {
-    console.log (data)
-    res.render('listado-categorias', {categorias: data});
+router.post('/insertcat', (req, res) => {
+  const {name} = req.body;
+  console.log(name);
+  db.insertcategory(name)
+  .then(() => {
+     res.redirect('categorias')
   })
   .catch(err => {
-    res.render('listado-categorias', {categorias: []});
+    console.log(err);
   })
 });
 
-// Agregar producto
-router.get('/agregar-producto', (req, res) => {
-  db.obtenerCategorias()
-  .then(data => {
-    console.log (data)
-    res.render('agregar-producto', {categorias: data});
-  })
-  .catch(err => {
-    res.render('agregar-producto', {categorias: []});
-  })
-});
-router.post('/productos', (req, res) => {
-  const { nombre, codigo, laboratorio, precio, cantidad, descripcion, categoria_id } = req.body;
-  db.insertarProducto(nombre, codigo, laboratorio, precio, cantidad, descripcion, categoria_id); // Función que inserta el producto en la base de datos
-  res.redirect('/agregar-producto');
-});
-
-// Agregar categoria
-router.get('/agregar-categoria', (req, res) => {
-  db.obtenerCategorias()
-  .then(data => {
-    console.log (data)
-    res.render('agregar-categoria', {categorias: data});
-  })
-  .catch(err => {
-    res.render('agregar-categoria', {categorias: []});
-  })
-});
-router.post('/categoria', (req, res) => {
-  const {nombre} = req.body;
-  db.insertarCategoria(nombre); // Función que inserta el producto en la base de datos
-  res.redirect('/agregar-categoria');
-});
-
-// Editar categoria
-router.get('/editar-categorias/:id', (req, res) => {
-  const id = req.params.id;
-  db.obtenercategoriaid(id)
-  .then(data => {
-    console.log (data)
-    res.render('editar-categorias', {categorias: data});
-  })
-  .catch(err => {
-    res.render('editar-categorias', {categorias: []});
-  })
-});
-router.post('/editar', (req, res) => {
-  const id = req.params.id;
-  const nombre = req.body.nombre;
-  db.editarCategoria(id, nombre); // Función que inserta el producto en la base de datos
-  res.redirect('/listado-categorias');
-});
-
-// Editar producto
-router.get('/editar-producto/:id', (req, res) => {
-  const id = req.params.id;
-  db.obtenerproductoid(id) 
-  .then(data => {
-    console.log (data)
-    res.render('editar-producto', {productos: data});
-  })
-  .catch(err => {
-    res.render('editar-producto', {productos: []});
-  })
-});
-router.post('/actualizar', (req, res) => {
-  const id = req.params.id;
-  const { nombre, codigo, laboratorio, precio, cantidad, descripcion} = req.body;
-  db.editarProducto(id, nombre, codigo, laboratorio, precio, cantidad, descripcion); // Función que inserta el producto en la base de datos
-  res.redirect('/listado');
-});
-
-// Agregar imagen
-router.get('/agregar-imagen/:id', (req, res) => {
-  db.obtenerCategorias()
-  .then(data => {
-    console.log (data)
-    res.render('agregar-imagen', {categorias: data});
-  })
-  .catch(err => {
-    res.render('agregar-categoria', {categorias: []});
-  })
-});
-router.post('/imagen', (req, res) => {
+router.post('/insertima', (req, res) => {
   const {url, producto_id, destacado} = req.body;
-  db.insertarImagen(url, producto_id, destacado); // Función que inserta el producto en la base de datos
-  res.redirect('/listado');
+  console.log(url, producto_id, destacado);
+  db.insertimagen(url, producto_id, destacado)
+  .then(() => {
+     res.redirect('imagenes')
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 
+
+//editar producto
+router.post('/edit_producto/', (req, res)=>{
+  const {id, code, name, lab, quantity, description, price, category_id,} = req.body;
+  db.updateproducto(id, code, name, lab, quantity, description, price, category_id)
+  .then(() =>{
+    res.redirect('/productos');
+  })
+  .catch(err =>{
+    console.log(err);
+
+  })
+});
+
+router.post('/editcat/', (req, res)=>{
+  const {id, name} = req.body;
+  db.updatecategory(id, name)
+  .then(() =>{
+    res.redirect('/categorias');
+  })
+  .catch(err =>{
+    console.log(err);
+
+  })
+});
+
+
+
+router.post('/editima/', (req, res)=>{
+  const {id, url, producto_id, destacado} = req.body;
+  db.updateimagen(id, url, producto_id, destacado)
+  .then(() =>{
+    res.redirect('/imagenes');
+  })
+  .catch(err =>{
+    console.log(err);
+
+  })
+});
+
+
+
+router.get('/edit_producto/:id', (req, res)=>{
+  const id = req.params.id
+  db.getproductoID(id)
+  .then(data =>{
+    console.log(data)
+    res.render('edit_producto', {producto: data[0]})
+  })
+    .catch(err =>{
+      console.log(err);
+      res.render('edit_producto', {producto: []})
+    }) 
+
+
+    
+})
+
+router.get('/editima/:id', (req, res)=>{
+  const id = req.params.id
+  db.getimagenID(id)
+  .then(data =>{
+    console.log(data)
+    res.render('editima', {imagen: data[0]})
+  })
+    .catch(err =>{
+      console.log(err);
+      res.render('editima', {imagen: []})
+    }) 
+
+
+    
+})
+
+router.get('/editcat/:id', (req, res)=>{
+  const id = req.params.id
+  db.getcategoryID(id)
+  .then(data =>{
+    console.log(data)
+    res.render('editcat', {category: data[0]})
+  })
+    .catch(err =>{
+      console.log(err);
+      res.render('editcat', {category: []})
+    }) 
+
+
+    
+})
+
+router.get('/delete/:id', (req, res)=>{
+  const id = req.params.id;
+  db.deleteproducto(id)
+    .then(() => {
+    res.redirect('/productos');
+  })
+  .catch(err => {
+  console.log(err);
+  });
+})
+
+router.get('/administrar', (req, res) =>{
+  res.render('administrar')
+})
+
+router.get('/categorias', (req, res) =>{
+  res.render('categorias')
+})
+
+router.get('/deletecat/:id', (req, res)=>{
+  const id = req.params.id;
+  db.deletecategory(id)
+    .then(() => {
+    res.redirect('/categorias');
+  })
+  .catch(err => {
+  console.log(err);
+  });
+})
+
+router.get('/deleteima/:id', (req, res)=>{
+  const id = req.params.id;
+  db.deleteimagen(id)
+    .then(() => {
+    res.redirect('/imagenes');
+  })
+  .catch(err => {
+  console.log(err);
+  });
+})
 
 
 
